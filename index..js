b702aabf45388e5d79acec72eeda411dd12bdd99
Jorge velasquez <168779014+Jorgevelas@users.noreply.github.com>
2025-12-22 10:34:02 -0500

    Update index..js

--- index..js
+++ index..js
@@ -1,20 +1,20 @@
-// Selecciona los elementos del DOM necesarios para el funcionamiento del generador
+
 const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
 
-// Evento de clic en el botón para generar el código QR
+
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
-    // Verifica si el valor ingresado es válido o si es igual al valor anterior
+  
     if(!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generando Código QR";
-    // Genera el código QR utilizando la API
+
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-    // Evento cuando la imagen ha cargado completamente
+    
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Código QR Generado";
@@ -29,3 +29,4 @@ qrInput.addEventListener("keyup", () => {
         preValue = "";
     }
 });
+
